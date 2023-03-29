0f25be67870d3730ecd556f49053afe0a4aaaf12
Megan <mmeiser19@gmail.com>
2023-03-29 18:13:19 -0400

    Added comments for clarity

--- msgq.c
+++ msgq.c
@@ -5,20 +5,18 @@
 #include "msgq.h"
 
 /*
- * You must design and implement struct msgq, which must involve dynamically allocated memory. My solution
- * includes a linked list of messages, where send_msg mallocs memory for the data structure and places it on the
- * end of the linked list. The data structure has a char* pointer to the message. The heap memory for the message
- * is allocated using strdup. recv_msg removes the head from the linked list and returns the char* pointer to the message
+ * Struct msgq is a message queue consisting the current number of messages, the maximum number of messages, an array
+ * to hold said messages, and a lock to protect the message queue
  */
 struct msgq {
-    int num_msgs; //current number of messages
-    int max_msgs; //maximum number of messages
-    char **messages; //array of messages
+    int num_msgs; // Current number of messages
+    int max_msgs; // Maximum number of messages
+    char **messages; // Array of messages
     pthread_mutex_t lock;
 };
 
 /*
- * initializes a message queue and returns a pointer to a struct msgq. The parameter num_msgs is the maximum number
+ * Initializes a message queue and returns a pointer to a struct msgq. The parameter num_msgs is the maximum number
  * of messages that may be in the message queue. The returned pointer is used in the other API functions
  */
 struct msgq *msgq_init(int num_msgs) {
@@ -29,55 +27,53 @@ struct msgq *msgq_init(int num_msgs) {
 }
 
 /*
- * places msg on message queue mq. mq is returned from msgq_init. msgq_send must make a copy of msg on the heap.
+ * Places msg on message queue mq. mq is returned from msgq_init. msgq_send must make a copy of msg on the heap.
  * If mq has num_msgs in it; then msgq_send blocks until there is room for msg. msgq_send returns 1 for success and
  * -1 for failure
  */
 int msgq_send(struct msgq *mq, char *msg) {
     pthread_mutex_lock(&mq->lock);
     char *msg_copy = strdup(msg);
-    if (mq->num_msgs == 0) {
+    if (mq->num_msgs == 0) { // If there are no messages in the queue, add the message
         mq->messages = malloc(sizeof(char*));
         mq->messages[0] = msg_copy;
         mq->num_msgs++;
     }
-    else if (mq->num_msgs == mq->max_msgs) {
+    else if (mq->num_msgs == mq->max_msgs) { // If the queue is full, print error message
         printf("Message queue is full\n");
         pthread_mutex_unlock(&mq->lock);
-        return -1; //msg was not sent
+        return -1; // Msg was not sent
     }
-    else {
+    else { // If there are messages in the queue but th queue is not full, add the message
         mq->messages = realloc(mq->messages, sizeof(char*) * (mq->num_msgs + 1));
         mq->messages[mq->num_msgs] = msg_copy;
         mq->num_msgs++;
     }
-    //check if msg was sent
+    // Check if msg was sent
     for (int i = 0; i < mq->max_msgs; i++) {
         if (mq->messages[i] == msg_copy) {
             pthread_mutex_unlock(&mq->lock);
-            return 1; //msg was sent
+            return 1; // Msg was sent
         }
     }
     printf("Message was not sent\n");
     pthread_mutex_unlock(&mq->lock);
-    return -1; //msg was not sent
+    return -1; // Msg was not sent
 }
 
 /*
- * returns a message from mq. mq is returned from msgq_init. The returned message is on the heap. The function that
+ * Returns a message from mq. mq is returned from msgq_init. The returned message is on the heap. The function that
  * receives the message can free it when it is no longer needed
  */
-//does there need to be some sort of yield here?
 char *msgq_recv(struct msgq *mq) {
-    //return a message from mq
-    if (mq->num_msgs == 0) {
+    if (mq->num_msgs == 0) { // If there are no messages in the queue, print error message
         printf("No messages to receive\n");
         return NULL;
     }
-    else {
+    else { // If there are messages in the queue, return the first message
         pthread_mutex_lock(&mq->lock);
         char *msg = mq->messages[0];
-        for (int i = 0; i < mq->num_msgs; i++) { //shift all messages down
+        for (int i = 0; i < mq->num_msgs; i++) { // Shift all messages down
             mq->messages[i] = mq->messages[i+1];
         }
         mq->num_msgs--;
@@ -87,8 +83,7 @@ char *msgq_recv(struct msgq *mq) {
 }
 
 /*
- * returns the number of messages on mq. mq is returned from msgq_init. Due to the nature of threads and interrupts,
- * the length returned may be incorrect by the time it is used
+ * Returns the number of messages on mq. mq is returned from msgq_init
  */
 int msgq_len(struct msgq *mq) {
     pthread_mutex_lock(&mq->lock);
@@ -98,7 +93,7 @@ int msgq_len(struct msgq *mq) {
 }
 
 /*
- * displays all of the messages in mq to stdout. mq is returned from msgq_init
+ * Displays all the messages in mq to stdout. mq is returned from msgq_init
  */
 void msgq_show(struct msgq *mq) {
     pthread_mutex_lock(&mq->lock);
